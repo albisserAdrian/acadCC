d652be0041a2c94427c9b746bc46665fc819d761
AdrianAlbisser <adrian.albisser@gmail.com>
2017-09-19 15:04:00 +1000

    Corrected path to scripts in batch file

--- RUN SCRIPTS.bat
+++ RUN SCRIPTS.bat
@@ -1,1 +1,1 @@
-FOR %%f IN ("%~dp0*.dwg") DO "C:\Program Files\Autodesk\AutoCAD 2017\accoreconsole.exe" /i "%%f" /s "%~dp0ucs_world.scr" /l en-US
+FOR %%f IN ("%~dp0*.dwg") DO "C:\Program Files\Autodesk\AutoCAD 2017\accoreconsole.exe" /i "%%f" /s "%~dp0scripts\purge.scr" /l en-US
