2f93edaf87a5b7090c72ec7864333f66c3a6e4d5
Adrian Albisser <adrian.albisser@gmail.com>
2019-07-11 13:16:24 +1000

    New script.

--- RUN-SCRIPTS.bat
+++ RUN-SCRIPTS.bat
@@ -1,1 +1,3 @@
+FOR %%f IN ("%~dp0*.dwg") DO "C:\Program Files\Autodesk\AutoCAD 2018\accoreconsole.exe" /i "%%f" /s "%~dp0scripts\laydel.scr" /l en-US
+FOR %%f IN ("%~dp0*.dwg") DO "C:\Program Files\Autodesk\AutoCAD 2018\accoreconsole.exe" /i "%%f" /s "%~dp0scripts\zoom_extents.scr" /l en-US
 FOR %%f IN ("%~dp0*.dwg") DO "C:\Program Files\Autodesk\AutoCAD 2018\accoreconsole.exe" /i "%%f" /s "%~dp0scripts\purge.scr" /l en-US
